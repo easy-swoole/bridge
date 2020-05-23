553693f2a1e253d42353c74f2b7caf65d571354c
如果的如果 <admin@fosuss.com>
2020-05-23 13:58:17 +0800

    up

--- src/Bridge.php
+++ src/Bridge.php
@@ -4,11 +4,15 @@
 namespace EasySwoole\Bridge;
 
 
+use EasySwoole\Component\Process\Socket\UnixProcessConfig;
 use Swoole\Server;
 
 class Bridge
 {
+    private $socketFile;
     private $container;
+    private $onStart;
+    private $onException;
 
     function __construct(Container $container = null)
     {
@@ -18,13 +22,56 @@ class Bridge
         $this->container = $container;
     }
 
+    function setOnStart(callable $call): Bridge
+    {
+        $this->onStart = $call;
+        return $this;
+    }
+
+    function setOnException(callable $call): Bridge
+    {
+        $this->onException = $call;
+        return $this;
+    }
+
     function getCommandContainer():Container
     {
         return $this->container;
     }
 
-    function attachServer(Server $server)
+    function attachServer(Server $server,string $serverName = 'EasySwoole')
     {
+        $config = new UnixProcessConfig();
+        $config->setSocketFile($this->socketFile);
+        $config->setProcessName("{$serverName}.Bridge");
+        $config->setProcessGroup("{$serverName}.Bridge");
+        $config->setArg([
+            'onStart'=>$this->onStart,
+            'container'=>$this->container,
+            'onException'=>$this->onException
+        ]);
+        $p = new BridgeProcess($config);
+        $server->addProcess($p->getProcess());
+    }
 
+
+
+    /**
+     * @return mixed
+     */
+    public function getSocketFile()
+    {
+        if(empty($this->socketFile)){
+            $this->socketFile = sys_get_temp_dir().'/bridge.sock';
+        }
+        return $this->socketFile;
+    }
+
+    /**
+     * @param mixed $socketFile
+     */
+    public function setSocketFile($socketFile): void
+    {
+        $this->socketFile = $socketFile;
     }
 }
--- src/BridgeProcess.php
+++ src/BridgeProcess.php
@@ -5,13 +5,69 @@ namespace EasySwoole\Bridge;
 
 
 use EasySwoole\Component\Process\Socket\AbstractUnixProcess;
+use EasySwoole\Socket\Tools\Protocol;
 use Swoole\Coroutine\Socket;
 
 class BridgeProcess extends AbstractUnixProcess
 {
-    function onAccept(Socket $socket)
+    private $container;
+    private $onException;
+
+    function run($arg)
     {
-        // TODO: Implement onAccept() method.
+        $this->container = $arg['container'];
+        $this->onException = $arg['onException'];
+        $onStart = $arg['onStart'];
+        if($onStart){
+            call_user_func($onStart,$this);
+        }
+        parent::run($arg);
     }
 
+    function onAccept(Socket $socket)
+    {
+        $data = Protocol::socketReader($socket, 3);
+        if ($data === null) {
+            $package = new  Package();
+            $package->setStatus(Package::STATUS_PACKAGE_ERROR);
+            Protocol::socketWriter($socket, serialize($package));
+            $socket->close();
+            return null;
+        }
+        /**
+         * @var $package Package
+         */
+        $package = unserialize($data);
+        $callback = $this->container->get($package->getCommand());
+        if (!$callback) {
+            $package = new Package();
+            $package->setStatus(Package::STATUS_COMMAND_NOT_EXIST);
+            $package->setArgs("command:{$package->getCommand()} is not exist");
+            Protocol::socketWriter($socket, serialize($package));
+            $socket->close();
+            return null;
+        }
+        $responsePackage = new Package();
+        try{
+            //结果在闭包中更改
+            $responsePackage->setStatus(Package::STATUS_SUCCESS);
+            call_user_func($callback,$package,$responsePackage,$socket);
+        }catch (\Throwable $throwable){
+            $responsePackage->setStatus(Package::STATUS_COMMAND_ERROR);
+            $responsePackage->setArgs($throwable->getMessage());
+            $this->onException($throwable);
+        } finally {
+            Protocol::socketWriter($socket,serialize($responsePackage));
+            $socket->close();
+        }
+    }
+
+    protected function onException(\Throwable $throwable, ...$args)
+    {
+        if($this->onException){
+            call_user_func($this->onException,$throwable);
+        }else{
+            throw $throwable;
+        }
+    }
 }
